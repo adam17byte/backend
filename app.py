923041da7226d57082f704c6cf2afb41154310f9
asep1122a <septiyanadam1@gmail.com>
2026-01-09 12:56:02 +0700

    update

--- app.py
+++ app.py
@@ -324,9 +324,10 @@ def log_request():
         print("BODY:", request.get_data())
 
 if __name__ == "__main__":
-    socketio.init_app(app)
+    
     socketio.run(
-        app,
-        host="0.0.0.0",
-        port=5000
-    )
+    app,
+    host="::",
+    port=5000
+)
+
